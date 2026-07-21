bc14c3e18ff0988fcd0c10aa22e2abcb96828d27
Tayfun <tayfuna766@gmail.com>
2026-07-21 16:18:40 +0300

    Add deterministic host JSON options

---
+++ hosts/windows-uia/HostJson.cs
@@ -0,0 +1,16 @@
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Ergaxiom.WindowsUiaHost;
+
+public static class HostJson
+{
+    public static JsonSerializerOptions Options { get; } = new()
+    {
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+        PropertyNameCaseInsensitive = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        WriteIndented = false,
+    };
+}
